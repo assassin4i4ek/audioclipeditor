13e0db51960bec1ce8d84997292aa095a159ce5a
Ruslan <yaneoyeah@gmail.com>
2021-08-09 20:38:01 +0300

    fixed concurrency with reentrant lock

--- src/main/kotlin/model/ClipUtilizer.kt
+++ src/main/kotlin/model/ClipUtilizer.kt
@@ -1,22 +1,49 @@
 package model
 
+import java.util.*
 import java.util.concurrent.LinkedBlockingQueue
+import java.util.concurrent.locks.ReentrantLock
 import javax.sound.sampled.Clip
+import kotlin.concurrent.withLock
 
 class ClipUtilizer {
-    private val blockingQueue = LinkedBlockingQueue<Clip>()
+    private var isUtilizerClosed = false
+    private val lock = ReentrantLock()
+    private val lockCondition = lock.newCondition()
+    private val utilizationQueue: MutableList<Clip> = LinkedList()
+
     private val utilizerThread = Thread {
-        val clip = blockingQueue.take()
-        clip.stop()
-        clip.flush()
-        clip.close()
-    }.apply { isDaemon = true }
+        while (true) {
+            lock.withLock {
+                lockCondition.await()
+                for (clip in utilizationQueue) {
+                    clip.stop()
+                    clip.flush()
+                    clip.close()
+                }
+                utilizationQueue.clear()
+                if (isUtilizerClosed) {
+                    return@Thread
+                }
+            }
+        }
+    }.apply {
+        isDaemon = true
+        start()
+    }
 
     fun utilizeClip(clip: Clip) {
-        blockingQueue.add(clip)
+        lock.withLock {
+            utilizationQueue.add(clip)
+            lockCondition.signal()
+        }
     }
 
     fun close() {
-        utilizerThread.interrupt()
+        lock.withLock {
+            isUtilizerClosed = true
+            lockCondition.signal()
+        }
+        utilizerThread.join()
     }
 }

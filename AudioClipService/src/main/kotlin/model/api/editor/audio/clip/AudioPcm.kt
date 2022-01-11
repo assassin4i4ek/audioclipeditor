b69580ae34c07865625a2026819f36f81c136381
Ruslan <yaneoyeah@gmail.com>
2022-01-11 11:13:10 +0200

    small change in AudioPcm interface

--- AudioClipService/src/main/kotlin/model/api/editor/audio/clip/AudioPcm.kt
+++ AudioClipService/src/main/kotlin/model/api/editor/audio/clip/AudioPcm.kt
@@ -3,12 +3,11 @@ package model.api.editor.audio.clip
 interface AudioPcm {
     val sampleRate: Int
     val numChannels: Int
-    val bytesPerSample get() = 2 // Short.SIZE_BYTES
 
     fun toPcmBytePosition(us: Long): Long {
-        return (1e-6 * us * sampleRate).toLong() * numChannels * bytesPerSample
+        return (1e-6 * us * sampleRate).toLong() * numChannels * 2 /*Short.SIZE_BYTES*/
     }
     fun toUs(pcmBytePosition: Long) : Long {
-        return (1e6 * pcmBytePosition / (sampleRate * numChannels * bytesPerSample)).toLong()
+        return (1e6 * pcmBytePosition / (sampleRate * numChannels * 2 /*Short.SIZE_BYTES*/)).toLong()
     }
 }
